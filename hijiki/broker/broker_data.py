140976b1764fd95bea0b5429771a562d12cd6249
Leandro Vilson Battisti <asengardeons@hotmail.com>
2023-10-08 18:07:31 -0300

    update main worflow to publish in pypi

--- hijiki/broker/broker_data.py
+++ hijiki/broker/broker_data.py
@@ -1,8 +1,14 @@
 import os
 
+BROKER_PORT = "BROKER_PORT"
+BROKER_PWD = "BROKER_PWD"
+BROKER_USERNAME = "BROKER_USERNAME"
+BROKER_SERVER = "BROKER_SERVER"
+
+
 def get_broker_url():
-    server = os.environ["BROKER_SERVER"] if "BROKER_SERVER" in os.environ else None
-    username = os.environ["BROKER_USERNAME"] if "BROKER_USERNAME" in os.environ else None
-    pwd = os.environ["BROKER_PWD"] if "BROKER_PWD" in os.environ else None
-    port = os.environ["BROKER_PORT"] if "BROKER_PORT" in os.environ else "5672"
+    server = os.environ[BROKER_SERVER] if BROKER_SERVER in os.environ else None
+    username = os.environ[BROKER_USERNAME] if BROKER_USERNAME in os.environ else None
+    pwd = os.environ[BROKER_PWD] if BROKER_PWD in os.environ else None
+    port = os.environ[BROKER_PORT] if BROKER_PORT in os.environ else "5672"
     return f'amqp://{username}:{pwd}@{server}:{port}' if server else 'amqp://rabbitmq:rabbitmq@localhost:5672'
--- tests/test_hijiki.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from hijiki.hijiki import add
-
-
-class TestMyModule(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
