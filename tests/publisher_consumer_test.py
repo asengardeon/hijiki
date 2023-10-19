743be97d5d30533fe496dbdab5a48942d68f2a31
battisti <asengardeons@hotmail.com>
2023-10-19 09:18:53 -0300

    start docker-compose on githubactions workflow

--- tests/publisher_consumer_test.py
+++ tests/publisher_consumer_test.py
@@ -8,53 +8,53 @@ from hijiki.broker.hijiki_rabbit import HijikiQueueExchange, HijikiRabbit
 from hijiki.publisher.Publisher import Publisher
 
 result_event_list = []
-container = (DockerContainer("rabbitmq:3-management-alpine") \
-    .with_bind_ports(15672, 15672) \
-    .with_bind_ports(5672, 5672) \
-    .with_name("rabbit_test_container") \
-    .with_env("RABBITMQ_DEFAULT_USER", "rabbitmq") \
-    .with_env("RABBITMQ_DEFAULT_PASS", "rabbitmq").start())
-class Runner():
+# container = (DockerContainer("rabbitmq:3-management-alpine").with_bind_ports(15672, 15672)
+#              .with_bind_ports(5672, 5672)
+#              .with_name("rabbit_test_container")
+#              .with_env("RABBITMQ_DEFAULT_USER", "rabbitmq")
+#              .with_env("RABBITMQ_DEFAULT_PASS","rabbitmq")).start()
 
+
+class Runner():
     qs = [HijikiQueueExchange('teste1', 'teste1_event'), HijikiQueueExchange('teste2', 'teste2_event')]
     gr = HijikiRabbit().with_queues_exchange(qs) \
         .with_username("rabbitmq") \
         .with_password("rabbitmq") \
-        .with_host(container.get_container_host_ip()) \
+        .with_host("localhost") \
         .with_port(5672) \
         .build()
+
+    threads = []
+
     @gr.task(queue_name="teste1")
     def internal_consumer(self):
         print("consumiu")
         result_event_list.append('recebeu evento')
 
     def run(self):
-        threads = []
         t = threading.Thread(target=self.gr.run)
-        threads.append(t)
+        self.threads.append(t)
         t.start()
 
     def __del__(self):
-        self.gr.close()
         super()
 
 
 class TestPublisherConsumer(unittest.TestCase):
     runner = None
+
     def setUp(self):
         if not self.runner:
             self.runner = Runner()
             self.runner.run()
+            self.pub = Publisher("localhost", "rabbitmq", "rabbitmq", 5672)
 
     def test_publish_a_message(self):
-        pub = Publisher("localhost", "rabbitmq", "rabbitmq", 5672)
-        pub.publish_message('teste1_event', '{"value": "Esta é a mensagem"}')
-
+        self.pub.publish_message('teste1_event', '{"value": "Esta é a mensagem"}')
 
     def test_consume_a_message(self):
-        pub = Publisher("localhost", "rabbitmq", "rabbitmq", 5672)
+        self.pub = Publisher("localhost", "rabbitmq", "rabbitmq", 5672)
         time.sleep(5)
-        pub.publish_message('teste1_event', '{"value": "Esta é a mensagem"}')
+        self.pub.publish_message('teste1_event', '{"value": "Esta é a mensagem"}')
         time.sleep(1)
         self.assertEqual(len(result_event_list), 1)
-
