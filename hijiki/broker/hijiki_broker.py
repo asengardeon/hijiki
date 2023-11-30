5cf0c1f5ae211ade5a393d3ef8a16cb84172ff9c
battisti <asengardeons@hotmail.com>
2023-11-30 16:00:20 -0300

    Added  cluster connection support to Publisher and Consumer

--- hijiki/broker/hijiki_broker.py
+++ hijiki/broker/hijiki_broker.py
@@ -16,13 +16,9 @@ class HijikiBroker:
             dead_letter_queue = Queue(f'dlq_{name}', dlx, routing_key='*', queue_arguments={'x-queue-type': 'quorum'})
             dead_letter_queue.bind(self.celery_broker.broker_connection()).declare()
 
-    def __init__(self, app_name, host, username, password, port):
-        init_os_environ(host, username, password, port)
+    def __init__(self, app_name, host, username, password, port, cluster_hosts):
+        init_os_environ(host, username, password, port, cluster_hosts)
         self.celery_broker = Celery(app_name, broker=get_broker_url(), set_as_current=True)
-
 
     def get_celery_broker(self):
         return self.celery_broker
-
-    def bind_queues(self, queues_names):
-        self.bind_queues(queues_names)
