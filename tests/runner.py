5b10e719a4421b960665a147822f410a37b30c7d
endersonMarques <emmf@cin.ufpe.br>
2024-04-03 15:49:45 -0300

    fix: add dlq consumers of queues and new extra queue

--- tests/runner.py
+++ tests/runner.py
@@ -3,10 +3,14 @@ import threading
 from hijiki.broker.hijiki_rabbit import HijikiQueueExchange, HijikiRabbit
 
 result_event_list = []
+result_event_list_dlq = []
 
 class Runner():
-    qs = [HijikiQueueExchange('teste1', 'teste1_event'),
-          HijikiQueueExchange('fila_erro', 'erro_event')]
+    qs = [
+        HijikiQueueExchange('teste1', 'teste1_event'),
+        HijikiQueueExchange('fila_erro', 'erro_event'),
+        HijikiQueueExchange('without_dlq', 'without_dlq'),
+    ]
     gr = HijikiRabbit().with_queues_exchange(qs) \
         .with_username("user") \
         .with_password("pwd") \
@@ -20,12 +24,29 @@ class Runner():
     def internal_consumer(data):
         print(f"consumiu o valor:{data}")
         result_event_list.append('recebeu evento')
+    
+    @gr.task(queue_name="teste1_dlq")
+    def internal_consumer_dlq(data):
+        print(f"consumiu o valor:{data}")
+        result_event_list_dlq.append('recebeu evento')
 
     @gr.task(queue_name="fila_erro")
-    def internal_consumer(data):
+    def internal_consumer_erro(data):
         print(f"consumiu o valor:{data}")
         result_event_list.append('recebeu evento')
         raise Exception("falhou")
+
+    @gr.task(queue_name="fila_erro_dlq")
+    def internal_consumer_erro_dlq(data):
+        print(f"consumiu o valor:{data}")
+        result_event_list_dlq.append('recebeu evento')
+    
+    @gr.task(queue_name="without_dlq")
+    def internal_consumer_extra(data):
+        print(f"consumiu o valor:{data}")
+        result_event_list.append('recebeu evento')
+        raise Exception("falhou")
+
 
     def run(self):
         t = threading.Thread(target=self.gr.run)
@@ -40,6 +61,13 @@ class Runner():
 
     def clear_results(self):
         result_event_list.clear()
+        result_event_list_dlq.clear()
 
     def get_results(self):
         return result_event_list
+
+    def get_results(self):
+        return result_event_list
+
+    def get_results_dlq(self):
+        return result_event_list_dlq
