87f174bed8f0f30e275fd50bc4c4fa9ab1c6014b
battisti <asengardeons@hotmail.com>
2024-05-10 11:38:12 -0300

    feat(heartbeat): fix heartbeat connection

--- hijiki/broker/hijiki_broker.py
+++ hijiki/broker/hijiki_broker.py
@@ -7,10 +7,13 @@ from hijiki.broker.broker_data import get_broker_url, init_os_environ
 
 class HijikiBroker:
     celery_broker = None
+    heartbeat = None
 
     def __init__(self, app_name, host, username, password, port, cluster_hosts, heartbeat: Optional[float] = 60):
         init_os_environ(host, username, password, port, cluster_hosts)
-        self.celery_broker = Celery(app_name, broker=get_broker_url(), set_as_current=True, heartbeat=heartbeat)
+        self.heartbeat = heartbeat
+        self.celery_broker = Celery(app_name, broker=get_broker_url(), set_as_current=True)
+
 
     def get_celery_broker(self):
         return self.celery_broker
